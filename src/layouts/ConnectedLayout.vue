3f9d3702736e3f383e84d7f5ca76c7ad53a9e085
Benoit DUMONT <benoit.dum74@gmail.com>
2025-08-07 19:42:39 +0200

    ♻️ | Added layouts for DRY principles

---
+++ src/layouts/ConnectedLayout.vue
@@ -0,0 +1,214 @@
+<template>
+  <div class="test">
+    <div class="content">
+      <div class="sidebar">
+        <div class="icons">
+          <img :src="getImage()" alt="Logo" />
+          <div class="icon-container">
+            <router-link :to="{ name: 'Accueil' }"><i class="fas fa-home"></i></router-link>
+            <router-link
+              :to="{
+                name: 'Profil',
+                params: { UserId: userStore.connectedUser!.id },
+              }"
+              ><i class="fas fa-user"></i
+            ></router-link>
+            <router-link :to="{ name: 'Settings' }"><i class="fas fa-cog"></i></router-link>
+            <router-link
+              v-if="userStore.connectedUser?.rank === 1 || userStore.connectedUser?.rank === 2"
+              :to="{ name: 'Home Dashboard' }"
+              ><i class="fas fa-tools"></i
+            ></router-link>
+          </div>
+        </div>
+        <div class="box">
+          <div class="up">
+            <div class="account">
+              <img :src="userStore.connectedUser?.avatar" :alt="t('ALTIMAGEPROFILE')" />
+              <i
+                v-if="menuDisplayed === false"
+                class="fas fa-sort-down"
+                @click="() => (menuDisplayed = !menuDisplayed)"
+              ></i>
+              <i v-else class="fas fa-sort-up" @click="() => (menuDisplayed = !menuDisplayed)"></i>
+            </div>
+            <transition name="logout">
+              <div v-if="menuDisplayed === true" class="logout">
+                <p @click="() => userStore.logout()">
+                  <i class="fas fa-sign-out-alt"></i>{{ t('LOGOUT') }}
+                </p>
+              </div>
+            </transition>
+          </div>
+          <slot />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Ref, ref } from 'vue';
+import { useI18n } from 'vue-i18n';
+
+import { useUserStore } from '@/stores/';
+import { getImage } from '@/utils';
+
+const userStore = useUserStore();
+const { t } = useI18n();
+
+const menuDisplayed: Ref<boolean> = ref(false);
+</script>
+
+<style scoped lang="scss">
+.content {
+  background-color: var(--app-background-color);
+  display: flex;
+}
+
+.sidebar {
+  background-color: var(--app-sidebar-color);
+  display: inline-flex;
+  width: 100%;
+  height: 100vh;
+}
+
+.icons {
+  display: inline-flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: space-between;
+  transition: color 450ms ease-in-out;
+  height: 70%;
+}
+
+.icons i {
+  font-size: 32px;
+  padding: 1vh;
+}
+
+.icons a {
+  transition: color 450ms ease-in-out;
+  color: var(--app-text-primary-color) !important;
+
+  &:hover {
+    opacity: 0.8;
+  }
+}
+
+.icons img {
+  margin: 2vh;
+  width: 85px;
+  height: 85px;
+  object-fit: cover;
+}
+
+.icon-container {
+  display: inline-flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: space-between;
+  transition: color 450ms ease-in-out;
+  height: 70%;
+}
+
+.box {
+  overflow: hidden;
+  position: relative;
+  width: 100%;
+}
+
+.up {
+  height: 10vh;
+  display: flex;
+  justify-content: flex-end;
+  padding-right: 4vh;
+  position: relative;
+}
+
+.account {
+  display: inline-flex;
+  align-items: center;
+  color: var(--app-text-primary-color);
+  padding: 2vh;
+}
+
+.account img {
+  width: 48px;
+  height: 48px;
+  object-fit: cover;
+  border-radius: 30px;
+  border: 1px solid #2d3036;
+}
+
+.account i {
+  padding-left: 1vh;
+  cursor: pointer;
+}
+
+.logout {
+  height: 5vh;
+  padding: 0 1.5vh;
+  position: absolute;
+  bottom: 0;
+  background: var(--app-sidebar-color);
+  z-index: 99999;
+  border-bottom-left-radius: 15px;
+  border-bottom-right-radius: 15px;
+  transform: translateY(100%);
+  cursor: pointer;
+}
+
+.logout i {
+  padding-right: 0.5vh;
+}
+
+.logout p {
+  color: var(--app-text-primary-color);
+}
+@media (max-width: 700px) {
+  .sidebar {
+    display: initial;
+    height: initial;
+  }
+
+  .icons {
+    height: initial;
+  }
+
+  .icon-container {
+    flex-direction: row;
+    position: fixed;
+    bottom: 0;
+    height: initial;
+    width: 100%;
+    left: 0;
+    right: 0;
+    padding: 2vh;
+    background: var(--app-sidebar-color);
+    z-index: 9999;
+    margin-top: 5vh;
+  }
+
+  .box {
+    position: initial;
+    padding-bottom: 10vh;
+  }
+
+  .up {
+    position: absolute;
+    top: 2vh;
+    right: 2vh;
+    padding-right: 0;
+  }
+
+  .logout {
+    height: 8vh;
+    padding: 0.5vh;
+    bottom: -2vh;
+    right: -1vh;
+    width: initial;
+    text-align: center;
+  }
+}
+</style>
